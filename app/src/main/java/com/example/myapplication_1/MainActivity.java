4f60db9c3e7676a3501b6e542a2f98db9f560ac2
Onicki <anne.nyberg89@gmail.com>
2019-10-04 10:08:38 +0300

    jotain

---
+++ app/src/main/java/com/example/myapplication_1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
+}
